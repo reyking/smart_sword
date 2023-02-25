12259e307ed0256487b977387529c72bcb62515e
Hans Raddatz Garcia <hans@pruebas.hansraddatz.com>
2023-02-24 23:38:25 -0300

    [refactor] limpeza de comentarios

--- web_listener/src/index.js
+++ web_listener/src/index.js
@@ -28,36 +28,6 @@ server.listen(HTTP_PORT, () => {
 
 const server_socket = net.createServer(socketConn);
 
-// const server_socket = net.createServer((socket) => {
-//   console.log("Client connected");
-//   socket.on("data", (data) => {
-//       const strData = data.toString();
-//       console.log(`Received: ${strData}`);
-
-//       const command = strData.split(",");
-//       const operator = command[0];
-//       const operand1 = parseFloat(command[1]);
-//       const operand2 = parseFloat(command[2]);
-//       let result;
-//       switch (operator) {
-//           case "add":
-//               result = operand1 + operand2;
-//               break;
-//           case "sub":
-//               result = operand1 - operand2;
-//               break;
-//       }
-//       socket.write(result.toString());
-//   });
-
-//   socket.on("end", () => {
-//       console.log("Client disconnected");
-//   });
-
-//   socket.on("error", (error) => {
-//       console.log(`Socket Error: ${error.message}`);
-//   });
-// });
 
 server_socket.on("error", (error) => {
   console.log(`server_socket Error: ${error.message}`);
